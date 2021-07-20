f617609c7275c67d249794bce7b7e1663d8bf78d
Alok kumar <alokrajnand@gmail.com>
2021-07-20 13:03:50 +0530

    last

--- app_platform/settings.py
+++ app_platform/settings.py
@@ -26,11 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG =  os.getenv("DEBUG","True")
-if os.getenv("DEBUG","")!= "":
-    DEBUG = False
-else:
-    DEBUG = True
+DEBUG =  os.getenv("DEBUG","False") == "True"
+
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
